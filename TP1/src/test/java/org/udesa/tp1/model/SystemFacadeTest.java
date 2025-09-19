17fb98356b722611200e41f3bf00edd945c1c690
juan <jkaplan@udesa.edu.ar>
2025-09-19 19:50:52 -0300

    mas cosas

--- TP1/src/test/java/org/udesa/tp1/model/SystemFacadeTest.java
+++ TP1/src/test/java/org/udesa/tp1/model/SystemFacadeTest.java
@@ -4,16 +4,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
-import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SystemFacadeTest {
+    public static Integer giftCardId1 = 100;
+    public static Integer giftCardId2 = 250;
+    public static Integer invalidGiftCardId = 0;
 
     SystemFacade systemFacade;
-    GiftCard giftCard100;
-    GiftCard giftCard250;
+    GiftCard giftCard1;
+    GiftCard giftCard2;
     @BeforeEach
     public void beforeEach() { systemFacade = systemFacade();}
 
@@ -29,33 +31,33 @@ public class SystemFacadeTest {
 
     @Test
     public void test03ValidUserClaimsValidGiftCard(){
-        systemFacade.claimGiftCard(systemFacade.login("John", "Jpass"), 100);
-        assertEquals("John", giftCard100.owner());
+        systemFacade.claimGiftCard(systemFacade.login("John", "Jpass"), giftCardId1);
+        assertEquals("John", giftCard1.owner());
     }
 
     @Test
     public void test04ValidUserClaimsInvalidGiftCard(){
-        assertThrowsLike(()-> systemFacade.claimGiftCard(systemFacade.login("John", "Jpass"), 0), SystemFacade.InvalidGiftCardSelectedError);
+        assertThrowsLike(()-> systemFacade.claimGiftCard(systemFacade.login("John", "Jpass"), invalidGiftCardId), SystemFacade.InvalidGiftCardSelectedError);
     }
 
     @Test
     public void test05ValidUserClaimsAlreadyClaimedGiftCard(){
-        systemFacade.claimGiftCard(systemFacade.login("John", "Jpass"), 100);
-        assertThrowsLike(()-> systemFacade.claimGiftCard(systemFacade.login("Paul", "Ppass"), 100), GiftCard.GiftCardIsClaimedError);
+        systemFacade.claimGiftCard(systemFacade.login("John", "Jpass"), giftCardId1);
+        assertThrowsLike(()-> systemFacade.claimGiftCard(systemFacade.login("Paul", "Ppass"), giftCardId1), GiftCard.GiftCardIsClaimedError);
     }
 
     @Test
     public void test06ValidUserChecksBalanceOfGiftCard(){
         Integer token = systemFacade.login("John", "Jpass");
-        assertEquals(100, systemFacade.claimGiftCard(token, 100).checkBalance(token, 100));
+        assertEquals(100, systemFacade.claimGiftCard(token, giftCardId1).checkBalance(token, giftCardId1));
     }
 
     private SystemFacade systemFacade() {
-        giftCard100 = new GiftCard(100);
-        giftCard250 = new GiftCard(250);
+        giftCard1 = new GiftCard(100);
+        giftCard2 = new GiftCard(250);
 
         return new SystemFacade(Map.of( "John", "Jpass", "Paul", "Ppass" ),
-                                Map.of(100 , giftCard100, 250, giftCard250),
+                                Map.of(giftCardId1 , giftCard1, giftCardId2, giftCard2),
                                 new Clock());
     }
 
